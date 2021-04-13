4092aa5413328dc69ae554fe17fbd3f7181d0887
Анатолий Андреев <anatoliiandreev19@gmail.com>
2021-04-14 01:07:17 +0300

    hw3

--- main.js
+++ main.js
@@ -1,52 +1,108 @@
+const $div_arenas = document.querySelector('.arenas');
+const $randomButton = document.querySelector('.button');
+
 player1 = {
+    player: 1,
     name: 'Scorpio',
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: 'knife',
-    attack: function (){
-        console.log(this.name+ ' Fight...');
+    attack: function (name){
+        console.log(name+ ' Fight...');
     }
 };
 
 player2 = {
+    player: 2,
     name: 'Kitana',
-    hp: 100,
+    hp: 80,
     img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
     weapon: 'gun',
-    attack: function (){
-        console.log(this.name+ ' Fight...');
+    attack: function (name){
+        console.log(name+ ' Fight...');
     }
 };
 
-function createPlayer(player,name){
-    const $div = document.createElement('div');
-    $div.classList.add(name);
-    document.body.appendChild($div);
+function createElement(tag, className){
+   const $tag = document.createElement(tag);
+   if (className){
+       $tag.classList.add(className);
+   }
+    return $tag;
+}
 
-    const $div_progressbar = document.createElement('div');
-    $div.appendChild($div_progressbar);
+function createPlayer(player){
 
-    const $div_life = document.createElement('div');
+    const $div = createElement('div','player'+player.player);
+    const $div_progressbar = createElement('div','progressbar');
+    const $div_life = createElement('div','life');
+    const $div_name = createElement('div','name');
+    const $div_character = createElement('div','character');
+    const $img = createElement('img');
+
+    $div.classList.add('player'+player.player);
+
     $div_life.style.width = player.hp + '%';
-    $div_progressbar.appendChild($div_life);
 
-    const $div_name = document.createElement('div');
     $div_name.innerText = player.name;
+
+    $img.src = player.img;
+
+    $div_progressbar.appendChild($div_life);
     $div_progressbar.appendChild($div_name);
 
-
-    const $div_character = document.createElement('div');
-    const $img = document.createElement('img');
-    $img.src = player.img;
-//$img.src = 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif';
     $div_character.appendChild($img);
 
     $div.appendChild($div_character);
+    $div.appendChild($div_progressbar);
 
-    $div_arenas = document.querySelector('.arenas');
-    $div_arenas.appendChild($div);
+    return $div;
 }
 
-createPlayer(player1,'player1');
-createPlayer(player2,'player2');
+function playerLose(name){
+    const $loseTitle = createElement('div','loseTitle');
+
+    if (name = player1.name){
+        WinnerName = player2.name;
+    }
+    else{
+        WinnerName = player1.name;
+    }
+
+    $loseTitle.innerText = WinnerName + ' won';
+
+    return $loseTitle;
+}
+
+function changeHP(player) {
+
+    const $playerLife = document.querySelector('.player'+ player.player+' .life');
+
+    hit = Math.ceil(Math.random() * 20) ;
+    if (hit == 0){
+        hit = 1;
+    }
+
+    player.hp -= hit;
+
+    if (player.hp < 0){
+        $playerLife.style.width = 0 + '%';
+    } else {
+        $playerLife.style.width = player.hp + '%';
+    }
+
+    if (player.hp < 0){
+        $div_arenas.appendChild(playerLose(player.name));
+        $randomButton.disabled = true;
+    }
+}
+
+$randomButton.addEventListener('click',function(){
+
+    changeHP(player1);
+    changeHP(player2);
+});
+
+$div_arenas.appendChild(createPlayer(player1));
+$div_arenas.appendChild(createPlayer(player2));
 
