556de6712568bee6fcd01384eb7b14de936f6274
Анатолий Андреев <anatoliiandreev19@gmail.com>
2021-04-18 17:41:00 +0300

    Delete example.js

--- example.js
+++
@@ -1,7 +0,0 @@
-const date = new Date();
-console.dir(date);
-
-logTime =  date.getFullYear() +":"+(date.getMonth()+1)+":"+date.getDate()+":"+
-    date.getHours() +":"+date.getMinutes()+":"+date.getSeconds();
-
-console.log(logTime);
